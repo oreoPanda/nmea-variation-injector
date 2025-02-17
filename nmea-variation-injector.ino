98f4f986ce34cf1d09d592f87b3ca01fdb9372cf
Jonas Fuglsang-Petersen <legojonas@gmx.de>
2025-02-17 21:27:28 +0100

    RAM usage improvements

--- nmea-variation-injector.ino
+++ nmea-variation-injector.ino
@@ -1,5 +1,4 @@
 String inputString = "";      // a String to hold incoming data
-String outputString = "";
 String checksum = "";
 int indexx;
 bool stringComplete = false;  // whether the string is complete
@@ -7,11 +6,32 @@ bool stringComplete = false;  // whether the string is complete
 void setup() {
   // initialize serial:
   Serial.begin(9600);
-  // reserve 200 bytes for the inputString:
-  inputString.reserve(200);
+  // reserve sufficient bytes for the strings:
+  checksum.reserve(10);
+  inputString.reserve(300);
 }
 
-String calcChecksum(String data) {
+
+/*
+ * void calcChecksum(const String &data, String &result)
+ *
+ * This function evaluates the received NMEA 0183 sentence and sends it over the
+ * serial port. If the NMEA 0183 sentence is a $GPRMC sentence, it Inserts a
+ * fixed variation value into the sentence and recalculates the checksum. The
+ * old checksum is replaced by the new one and the new sentence is then sent over
+ * the serial port. Other nmea sentences remain untouched.
+ * Note: This function assumes that the variation is not already set in the
+ * received $GPRMC sentence and is not safe to use if the value is already set.
+ *
+ * @param args
+ * const String &data:  The part of the NMEA 0183 sentence of which the checksum shall be calculated.
+ * String &result:      The String to write the checksum into.
+ *
+ * @param returns
+ * nothing
+ *
+ */
+void calcChecksum(const String &data, String &result) {
   byte checksum = 0;
 
   for (int i = 0; i < data.length(); i++) {
@@ -21,32 +41,44 @@ String calcChecksum(String data) {
   char hexString[3];
   sprintf(hexString, "%02X", checksum);
 
-  return String(hexString);
+  result = hexString;
 }
 
+/*
+ * void loop()
+ *
+ * This function evaluates the received NMEA 0183 sentence and sends it over the
+ * serial port. If the NMEA 0183 sentence is a $GPRMC sentence, it Inserts a
+ * fixed variation value into the sentence and recalculates the checksum. The
+ * old checksum is replaced by the new one and the new sentence is then sent over
+ * the serial port. Other nmea sentences remain untouched.
+ * Note: This function assumes that the variation is not already set in the
+ * received $GPRMC sentence and is not safe to use if the value is already set.
+ *
+ * @param args
+ * none
+ *
+ * @param returns
+ * nothing
+ *
+ */
 void loop() {
-  // print the string when a newline arrives:  
   if (stringComplete) {
     if (inputString.startsWith("$GPRMC")) {
+      Serial.println(inputString);
       //scroll to the 10th comma
       indexx = 0;
       for (int i = 0; i < 10; i++) {
         indexx = inputString.indexOf(',', indexx+1);    //TODO handle the -1 as error
       }
-      //we now have the index of the 10th comma stored in the variable called index
-      outputString = inputString.substring(0, indexx+1);
-      outputString.concat("1.0,E");
-      //find the index of the 11th comma in the inputString. This should be just next to the 10th comma, unless a deviation is already given
-      indexx = indexx +4;
-      //append the rest of the inputString to the outputString
-      outputString.concat(inputString.substring(indexx));
-      //put the outputString onto the serial connection
-      checksum = calcChecksum(outputString);
-      outputString.remove(outputString.indexOf('*'));
-      outputString.concat('*');
-      outputString.concat(checksum);
-      outputString.concat("\r\n");
-      Serial.print(outputString);      
+      //we now have the index of the 10th comma stored in the variable called index. Remove everything after the 10th comma:
+      inputString.remove(indexx+1);
+      //add magnetic variation
+      inputString.concat("1.0,E*");
+      //add checksum
+      calcChecksum(inputString, checksum);
+      inputString.concat(checksum);
+      inputString.concat("\r\n");   
     }
     else {
       inputString.concat("\r\n");
@@ -59,6 +91,21 @@ void loop() {
   }
 }
 
+/*
+ * void serialEvent()
+ *
+ * Whenever a new byte is available on the serial port, this function reads
+ * the byte from the serial port and appends it to the global variable inputString.
+ * If the byte is a newline character ('\n' / decimal 10 / hexadecimal 0x10),
+ * it means that the end of an NMEA 0183 sentence was read and the global
+ * variable string_complete is set to true.
+ *
+ * @param args
+ * none
+ *
+ * @param returns
+ * nothing
+ */
 void serialEvent() {
   while (Serial.available()) {    
     // get the new byte:
